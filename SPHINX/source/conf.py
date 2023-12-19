a17a40caf940af0dfe3cc223821009282836baea
jbrunto <brunton.jonathan@epa.gov>
2023-12-19 10:25:09 -0500

    adding CMAQ Dev docs

---
+++ SPHINX/source/conf.py
@@ -0,0 +1,56 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+# These four objects provided with initial setup
+project = "CMAQ"
+copyright = '2023, CMAQ Developers'
+author = 'CMAQ Developers'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+# These three objects provided with initial setup - 'myst_parser' added later
+extensions = ['myst_parser']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# custom provides configuration values
+source_suffix = {
+        '.rst': 'restructuredtext',
+        '.txt': 'restructuredtext',
+        '.md': 'markdown',
+}
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# These two objects provided with initial setup
+import pydata_sphinx_theme
+html_theme = 'pydata_sphinx_theme'
+html_static_path = ['_static']
+
+html_sidebars = {
+        '**': 'globaltoc.html'
+        }
+
+
+html_theme_options = {
+    "github_url": "https://github.com/USEPA/CMAQ",
+    "logo": {
+        "image_light": "_static/CMAQ_Logo_2_inch.png",
+        "image_dark": "_static/CMAQ_Logo_2_inch.png",
+    }
+
+
+
+
+
+ }
---
+++ SPHINX/source/index.rst
@@ -0,0 +1,26 @@
+.. User's Guide documentation master file, created by
+   sphinx-quickstart on Tue Oct 24 15:16:14 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+CMAQ: The Community Multiscale Air Quality Modeling System
+==========================================================
+
+**CMAQ:** an active open-source development project of the U.S. EPA that consists of a suite of programs for conducting air quality model simulations. CMAQ combines current knowledge in atmospheric science and air quality modeling, multi-processor computing techniques, and an open source framework to deliver fast, technically sound estimates of ozone, particulates, toxics and acid deposition.  See <https://epa.gov/cmaq>.
+
+
+.. note::
+
+        Not the official CMAQ User's Guide
+
+
+
+.. toctree::        
+   :maxdepth: 3
+   :caption: Contents
+
+
+   Users_Guide/README.md
+   license.md
+
+
